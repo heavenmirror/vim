6952aaa3406d290e4f7ec5a98a4379cc7009bd68
zhangyi <106298938@qq.com>
2015-08-04 17:59:08 +0800

    fixed init

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 mv ~/.vimrc ~/.vimrc.bak
 ln ./.vimrc ~/.vimrc
 echo "complete ! Let\`s play !"
-git clone "https://github.com/gmarik/Vundle.vim.git" "~/.vim/bundle/Vundle.vim"
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
